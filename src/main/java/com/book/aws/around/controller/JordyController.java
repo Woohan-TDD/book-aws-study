3c05789a0cdd351c70f2f87ecbe4f492ddc3b17c
mintjordy <mintjordy@woowahan.com>
2021-03-01 21:06:31 +0900

    init: init project

---
+++ src/main/java/com/book/aws/around/controller/JordyController.java
@@ -0,0 +1,21 @@
+package com.book.aws.around.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class JordyController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello";
+    }
+
+    @ResponseBody
+    @RequestMapping("/health")
+    public String healthCheck() {
+        return "ok";
+    }
+}
