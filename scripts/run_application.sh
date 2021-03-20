b5bbf440394b4a345d8d87db7de3cd9b0f62905f
K.S.KIM <nullable@kakao.com>
2021-03-21 05:34:35 +0900

    chore: 불필요 삭제

--- scripts/run_application.sh
+++
@@ -1,7 +0,0 @@
-echo 'Run java web application'
-
-BASE_PATH=/var/www/book-aws-study
-BUILD_PATH=$(ls $BASE_PATH/build/libs/*.jar)
-JAR_NAME=$(basename "$BUILD_PATH")
-echo "> build 파일명: $JAR_NAME"
-nohup java -jar "$BUILD_PATH" > $BASE_PATH/nohup.out 2>&1 &
