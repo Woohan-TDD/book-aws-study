384d0bb01faa582988f87b45bebace0b8ed66a5f
mintjordy <mintjordy@woowahan.com>
2021-03-20 18:22:45 +0900

    fix: run_application_server.sh 스크립트 수정
    - application 동작을 위해 실행시킬 스크립트 경로 수정

--- scripts/run_application_server.sh
+++ scripts/run_application_server.sh
@@ -3,7 +3,7 @@ echo 'Running restart_server'
 echo '======================'
 
 BASE_PATH=/var/www/aws-exercise-a
-BUILD_PATH=$(ls $BASE_PATH/book-aws-study/build/libs/*.jar)
+BUILD_PATH=$(ls $BASE_PATH/build/libs/*.jar)
 JAR_NAME=$(basename "$BUILD_PATH")
 echo "> build 파일명: $JAR_NAME"
 nohup java -jar "$BUILD_PATH" &
