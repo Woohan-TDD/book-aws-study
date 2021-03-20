b99427f8e5db14e8c6537ef5427ec6cd46732178
mintjordy <mintjordy@woowahan.com>
2021-03-20 18:57:24 +0900

    fix: nohup.out을 표준 입출력으로 설정
    - 애플리케이션 동작시 timeout 에러 fix

--- scripts/run_application_server.sh
+++ scripts/run_application_server.sh
@@ -6,4 +6,4 @@ BASE_PATH=/var/www/aws-exercise-a
 BUILD_PATH=$(ls $BASE_PATH/build/libs/*.jar)
 JAR_NAME=$(basename "$BUILD_PATH")
 echo "> build 파일명: $JAR_NAME"
-nohup java -jar "$BUILD_PATH" &
+nohup java -jar "$BUILD_PATH" > $BASE_PATH/nohup.out 2>&1 &
