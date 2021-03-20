b40baa42b44cd81b94e3aa1fc24868c6e473de73
K.S.KIM <nullable@kakao.com>
2021-03-21 05:55:02 +0900

    fix: 어플리케이션 실행 스크립트 STDOUT 수정

--- scripts/run_application.sh
+++ scripts/run_application.sh
@@ -3,4 +3,4 @@ echo 'Run java web application'
 BASE_PATH=/var/www/book-aws-study
 BUILD_PATH=$(ls $BASE_PATH/build/libs/*.jar)
 
-nohup java -jar "$BUILD_PATH" &
+nohup java -jar "$BUILD_PATH" $BASE_PATH/nohup.out  /dev/null 2> /dev/null < /dev/null &
